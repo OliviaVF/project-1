7a092d6099a45d6db16a3c6cdae72be6cbb1dc46
OliviaVF <opvf22@gmail.com>
2017-02-11 15:13:29 +0000

    made shuffle, split deck, change arrays based on winner

--- js/app.js
+++ js/app.js
@@ -4,9 +4,24 @@ $(() => {
   const $p1Card = $('#p1');
   const $compCard = $('#comp');
   console.log(window.cards);
+  shuffle(window.cards);
 
-  const p1 = [];
-  const comp = [];
+  function shuffle(array) {
+    var i = 0
+    , j = 0
+    , temp = null;
+
+    for (i = array.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
+  const p1 = window.cards.slice(0, 12);
+  const comp = window.cards.slice(12, 24);
+
 
   const $p1Name = $p1Card.find('.name').text(p1[0].name);
   const $p1Heartache = $p1Card.find('.heartache .value').text(p1[0].heartache);
@@ -40,12 +55,16 @@ $(() => {
       compScore--;
       $yourScore.html(p1Score);
       $compScore.html(compScore);
+      p1.push(p1.splice(0,1)[0], comp.splice(0,1),[0]);
       console.log('winner');
     } else {
       p1Score--;
       $yourScore.html(p1Score);
       compScore++;
       $compScore.html(compScore);
+      comp.push(p1.splice(0,1)[0], comp.splice(0,1),[0]);
+      console.log(p1);
+      console.log(comp);
     }
   }
 
