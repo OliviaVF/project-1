6a8cd8d8a07fcc8283a301d2f482aab6278095cf
OliviaVF <opvf22@gmail.com>
2017-02-11 13:05:42 +0000

    made cards, timer, and score

--- js/app.js
+++ js/app.js
@@ -1,1 +1,72 @@
-console.log('JS loaded');
+$(() => {
+
+
+  const $p1Card = $('#p1');
+  const $compCard = $('#comp');
+  console.log(window.cards);
+
+  const p1 = [];
+  const comp = [];
+
+  const $p1Name = $p1Card.find('.name').text(p1[0].name);
+  const $p1Heartache = $p1Card.find('.heartache .value').text(p1[0].heartache);
+  const $p1Trauma = $p1Card.find('.trauma .value').text(p1[0].trauma);
+  const $p1sexAppeal = $p1Card.find('.sexAppeal .value').text(p1[0].sexAppeal);
+  const $compName = $compCard.find('.name').text(comp[0].name);
+  const $compHeartache = $compCard.find('.heartache .value').text(comp[0].heartache);
+  const $compTrauma = $compCard.find('.trauma .value').text(comp[0].trauma);
+  const $compSexAppeal = $compCard.find('.sexAppeal .value').text(comp[0].sexAppeal);
+
+  function fillCards() {
+    $p1Name;
+    $p1Heartache;
+    $p1Trauma;
+    $p1sexAppeal;
+    $compName;
+    $compHeartache;
+    $compTrauma;
+    $compSexAppeal;
+  }
+
+  let p1Score = 10;
+  let compScore = 10;
+  const $yourScore = $('#yourScore');
+  const $compScore = $('#compScore');
+
+  function compare() {
+    const category = $(this).attr('class');
+    if (p1[0][category] > comp[0][category]) {
+      p1Score++;
+      compScore--;
+      $yourScore.html(p1Score);
+      $compScore.html(compScore);
+      console.log('winner');
+    } else {
+      p1Score--;
+      $yourScore.html(p1Score);
+      compScore++;
+      $compScore.html(compScore);
+    }
+  }
+
+  const $timer = $('.timer');
+  let timeRemaining = 60;
+  let timerId = null;
+
+
+  function startTimer() {
+    timerId = setInterval(() => {
+      timeRemaining--;
+      $timer.text(timeRemaining);
+
+      if(timeRemaining === 0) {
+        clearInterval(timerId);
+        timeRemaining = 60;
+      }
+    }, 1000);
+  }
+
+  startTimer();
+  fillCards();
+  $('.card > div').on('click', compare);
+});
---
+++ js/cardData.js
@@ -0,0 +1,122 @@
+window.cards = [{
+  name: 'Ian Beale',
+  heartache: 99,
+  trauma: 99,
+  sexAppeal: 100
+},{
+  name: 'Pat Butcher',
+  heartache: 83,
+  trauma: 89,
+  sexAppeal: 90
+},{
+  name: 'Dot Cotton',
+  heartache: 70,
+  trauma: 76,
+  sexAppeal: 43
+},{
+  name: 'Bianca Jackson',
+  heartache: 78,
+  trauma: 96,
+  sexAppeal: 51
+},{
+  name: 'Phil Mitchell',
+  heartache: 47,
+  trauma: 97,
+  sexAppeal: 92
+},{
+  name: 'Grant Mitchell',
+  heartache: 49,
+  trauma: 79,
+  sexAppeal: 93
+},{
+  name: 'Peggy Mitchell',
+  heartache: 89,
+  trauma: 98,
+  sexAppeal: 94
+},{
+  name: 'Ricky Butcher',
+  heartache: 70,
+  trauma: 71,
+  sexAppeal: 30
+},{
+  name: 'Dirty Den',
+  heartache: 46,
+  trauma: 98,
+  sexAppeal: 94
+},{
+  name: 'Pauline Fowler',
+  heartache: 96,
+  trauma: 95,
+  sexAppeal: 63
+},{
+  name: 'Mick Carter',
+  heartache: 70,
+  trauma: 90,
+  sexAppeal: 99
+},{
+  name: 'Sonia Fowler',
+  heartache: 96,
+  trauma: 96,
+  sexAppeal: 81
+},{
+  name: 'Kat Slater',
+  heartache: 100,
+  trauma: 100,
+  sexAppeal: 89
+},{
+  name: 'Alfie Moon',
+  heartache: 89,
+  trauma: 93,
+  sexAppeal: 75
+},{
+  name: 'Big Mo',
+  heartache: 70,
+  trauma: 90,
+  sexAppeal: 90
+},{
+  name: 'David Wicks',
+  heartache: 69,
+  trauma: 71,
+  sexAppeal: 90
+},{
+  name: 'Kathy Beale',
+  heartache: 69,
+  trauma: 98,
+  sexAppeal: 68
+},{
+  name: 'Sharon Watts',
+  heartache: 89,
+  trauma: 94,
+  sexAppeal: 71
+},{
+  name: 'Shirley Carter',
+  heartache: 79,
+  trauma: 89,
+  sexAppeal: 97
+},{
+  name: 'Janine Butcher',
+  heartache: 79,
+  trauma: 88,
+  sexAppeal: 67
+},{
+  name: 'Billy Mitchell',
+  heartache: 73,
+  trauma: 87,
+  sexAppeal: 49
+},{
+  name: 'Max Branning',
+  heartache: 61,
+  trauma: 96,
+  sexAppeal: 29
+},{
+  name: 'Whitney Dean',
+  heartache: 98,
+  trauma: 97,
+  sexAppeal: 87
+},{
+  name: 'Frank Butcher',
+  heartache: 76,
+  trauma: 86,
+  sexAppeal: 98
+}
+];
